39e2000b041805665789e4d291b119483678c66d
achater0 <chaterabderrafia@gmail.com>
2026-07-17 18:24:18 +0100

    adjust letterSpacing

--- frontend/src/ContractVerify.jsx
+++ frontend/src/ContractVerify.jsx
@@ -169,7 +169,7 @@ export default function ContractVerify() {
       fontWeight: 'bolder',
       lineHeight: '1.1',
       display: 'inline-block',
-      letterSpacing: '1.2px'
+      letterSpacing: '1px'
     }}>
       Contrat : Validé (En Exécution)
     </b>
