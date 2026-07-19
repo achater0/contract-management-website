4aebec68c38923dfa971e8db84b6853d9106a7f5
achater0 <chaterabderrafia@gmail.com>
2026-07-19 13:23:42 +0100

    adjust the font

--- frontend/src/ContractVerify.jsx
+++ frontend/src/ContractVerify.jsx
@@ -168,7 +168,8 @@ export default function ContractVerify() {
       color: '#28a745',
       fontWeight: 'bolder',
       lineHeight: '1.1',
-      display: 'inline-block'
+      display: 'inline-block',
+      letterSpacing: '0.08px'
     }}>
       Contrat : Validé (En Exécution)
     </b>
